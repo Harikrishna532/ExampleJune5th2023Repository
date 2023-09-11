328f36b5163416ee8073a6d92b6b80a315e9ed19
Harikrishna532 <harikrishnaprasad55@gmail.com>
2023-09-11 21:34:40 +0530

    Recursion Method

---
+++ Web-Automation/src/p1/EvenNumberForLoop.java
@@ -0,0 +1,19 @@
+package p1;
+
+public class EvenNumberForLoop {
+
+	public static void main(String[] args) {
+		
+		{
+			for(int i=100;i<=200;i++)
+			{
+				if(i%2==0)
+				{
+					System.out.println(i);
+				}
+			}
+		}
+
+	}
+
+}
